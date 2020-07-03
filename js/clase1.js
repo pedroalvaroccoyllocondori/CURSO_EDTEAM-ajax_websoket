790231697f4dd0cd9c0ae762fe46adad6d5b64fa
alvaro <pedro_alvaro@outlook.com.pe>
2020-07-03 00:13:14 -0500

    creacion de un loader

--- js/clase1.js
+++ js/clase1.js
@@ -1,21 +1,31 @@
 const contenido = document.getElementById('mycontenido')
 const boton=document.getElementById('btncargarJSON')
-
+const cargar=document.getElementById('cargar')
+///ocultar
+cargar.style.display='none'
 
 boton.addEventListener('click',evt=>{
-    const xhr= new XMLHttpRequest()
-    console.log('objeto crado',xhr.readyState)
-
-    xhr.open('GET','/json',true)//url relativasi es el mismo dominio se usa /
-    console.log('objeto abierto',xhr.readyState)
+    cargar.style.display='block'
+    setTimeout(()=>{
+        const xhr= new XMLHttpRequest()
+        console.log('objeto crado',xhr.readyState)
     
-    xhr.addEventListener('load',e=>{
-        console.log(e.target)
-        console.log('objeto cargado',xhr.readyState)
-    })
+        xhr.open('GET','/json',true)//url relativasi es el mismo dominio se usa /
+        console.log('objeto abierto',xhr.readyState)
+        
+        xhr.addEventListener('load',e=>{
+    
+            console.log(e.target)
+            console.log('objeto cargado',xhr.readyState)
+            //ocultar 
+            cargar.style.display='none'
+        })
+    
+        ///realize la peticion
+    
+        xhr.send()
+        console.log('accion solicitado',xhr.readyState)
 
-    ///realize la peticion
-
-    xhr.send()
-    console.log('accion solicitado',xhr.readyState)
+    },10000)
+   
 })
